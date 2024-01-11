6f095c88b4f8169a55497023752afc76f2a41e35
Eduardo Arruda <eduardoarruda2001@outlook.com>
2024-01-11 19:43:10 -0300

    Band

---
+++ index.py
@@ -0,0 +1,4 @@
+print("Gerador de nome de banda")
+cidade = input("Coloque o nome da sua cidade: \n")
+pet = input("Coloque o nome do seu pet: \n")
+print("O nome da sua banda é: " + cidade + " " + pet)
