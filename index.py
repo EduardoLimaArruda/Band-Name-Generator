d6a3cfa45e5b4c3715298092fe7e599a6e10c542
Eduardo Arruda <eduardoarruda2001@outlook.com>
2024-01-12 21:00:52 -0300

    v2

--- index.py
+++ index.py
@@ -2,3 +2,5 @@ print("Gerador de nome de banda")
 cidade = input("Coloque o nome da sua cidade: \n")
 pet = input("Coloque o nome do seu pet: \n")
 print("O nome da sua banda é: " + cidade + " " + pet)
+
+enter = input("\n Aperte Enter para fechar")
